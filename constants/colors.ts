9d095052916e520efbf250563adc1684094e1209
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2025-04-27 18:01:21 +0300

    Add white and black color constants to Colors object in colors.ts for improved color management.

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,11 +1,15 @@
 const primary = '#e62838';
 const accent = '#09d116';
+const white = '#fff';
 const background = '#f5f5f5';
 const gray = '#999';
+const black = '#000';
 
 export const Colors = {
   primary,
   accent,
+  white,
   background,
   gray,
+  black,
 };
