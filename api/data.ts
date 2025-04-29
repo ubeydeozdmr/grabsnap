b7c5d4bcff5491597749fd3ba2e77fa31dd93699
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2025-04-29 23:20:02 +0300

    Add user data integration: Introduce user type and related functions to fetch user information. Update Car type to include seller details and enhance car data structure with additional attributes like description and features.

--- api/data.ts
+++ api/data.ts
@@ -1,6 +1,7 @@
 import cars from '../data/cars.json';
 import makes from '../data/makes.json';
 import models from '../data/models.json';
+import users from '../data/users.json';
 import { randomDelay } from './utils';
 
 type RawMake = {
@@ -18,29 +19,54 @@ type RawModel = {
 type RawCar = {
   id: number;
   model: number;
+  seller: number;
   image: string;
+  description: string;
   releaseYear: number;
   price: number;
   engineSize: number;
   mileage: number;
-  gearbox: 'auto' | 'manual';
+  gearbox: 'automatic' | 'semi-automatic' | 'manual';
   city: string;
   power: number;
   fuel: 'gasoline' | 'diesel' | 'electric' | 'hybrid';
-  bodyType: 'SUV' | 'compact-SUV' | 'sedan';
+  bodyType: 'SUV' | 'compact-SUV' | 'sedan' | 'coupe' | 'hatchback';
   color: string;
-  condition: 'perfect' | 'good' | 'bad';
+  condition: 'excellent' | 'good' | 'bad';
   isNew: boolean;
   datetime: string;
+  addFeats: string[];
+};
+
+type User = {
+  id: number;
+  phone: string;
+  email: string;
+  password: string;
+  fullName: string;
 };
 
 export type Make = RawMake;
 export type Model = Omit<RawModel, 'make'> & { make: Make };
-export type Car = Omit<RawCar, 'model'> & { model: Model };
+export type Car = Omit<RawCar, 'model' | 'seller'> & {
+  model: Model;
+  seller: User;
+};
 
 const _makes: Make[] = makes as Make[];
 const _models: RawModel[] = models as RawModel[];
 const _cars: RawCar[] = cars as RawCar[];
+const _users: User[] = users as User[];
+
+export async function getUsers(): Promise<User[]> {
+  await randomDelay();
+  return _users;
+}
+
+export async function getUserById(id: number): Promise<User> {
+  await randomDelay();
+  return _users.find((user) => user.id === id)!;
+}
 
 export async function getMakes(): Promise<Make[]> {
   await randomDelay();
@@ -88,9 +114,11 @@ export async function getCars(): Promise<Car[]> {
   const cars = await Promise.all(
     _cars.map(async (car) => {
       const model = await getModelById(car.model);
+      const seller = await getUserById(car.seller);
       return {
         id: car.id,
         image: car.image,
+        description: car.description,
         releaseYear: car.releaseYear,
         price: car.price,
         engineSize: car.engineSize,
@@ -104,7 +132,9 @@ export async function getCars(): Promise<Car[]> {
         condition: car.condition,
         isNew: car.isNew,
         datetime: car.datetime,
+        addFeats: car.addFeats,
         model,
+        seller,
       };
     }),
   );
@@ -113,35 +143,46 @@ export async function getCars(): Promise<Car[]> {
 
 export async function getCarsFromModel(modelId: number): Promise<Car[]> {
   const model = await getModelById(modelId);
-  return _cars
-    .filter((car) => car.model === modelId)
-    .map((car) => ({
-      id: car.id,
-      image: car.image,
-      releaseYear: car.releaseYear,
-      price: car.price,
-      engineSize: car.engineSize,
-      mileage: car.mileage,
-      gearbox: car.gearbox,
-      city: car.city,
-      power: car.power,
-      fuel: car.fuel,
-      bodyType: car.bodyType,
-      color: car.color,
-      condition: car.condition,
-      isNew: car.isNew,
-      datetime: car.datetime,
-      model,
-    }));
+  return Promise.all(
+    _cars
+      .filter((car) => car.model === modelId)
+      .map(async (car) => {
+        const seller = await getUserById(car.seller);
+        return {
+          id: car.id,
+          image: car.image,
+          description: car.description,
+          releaseYear: car.releaseYear,
+          price: car.price,
+          engineSize: car.engineSize,
+          mileage: car.mileage,
+          gearbox: car.gearbox,
+          city: car.city,
+          power: car.power,
+          fuel: car.fuel,
+          bodyType: car.bodyType,
+          color: car.color,
+          condition: car.condition,
+          isNew: car.isNew,
+          datetime: car.datetime,
+          addFeats: car.addFeats,
+          model,
+          seller,
+        };
+      }),
+  );
 }
 
 export async function getCarById(id: number): Promise<Car> {
   const car = _cars.find((car) => car.id === id)!;
   const model = await getModelById(car.model);
+  const seller = await getUserById(car.seller);
   return {
     id: car.id,
     model,
+    seller,
     image: car.image,
+    description: car.description,
     releaseYear: car.releaseYear,
     price: car.price,
     engineSize: car.engineSize,
@@ -155,5 +196,6 @@ export async function getCarById(id: number): Promise<Car> {
     condition: car.condition,
     isNew: car.isNew,
     datetime: car.datetime,
+    addFeats: car.addFeats,
   };
 }
