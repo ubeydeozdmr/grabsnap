43391f8d4f7d983c8a3c464812d87615546a59b5
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2025-05-01 15:00:11 +0300

    WIP: Refactor CarDetails component to enhance layout and improve readability. Consolidate repeated elements, add missing car attributes, and implement a new button for contacting the seller.

--- screens/CarDetails.tsx
+++ screens/CarDetails.tsx
@@ -34,109 +34,146 @@ export default function CarDetails({ route }: Props): JSX.Element {
   }
 
   return (
-    <ScrollView style={styles.container}>
-      <Image source={{ uri: car.image }} style={styles.image} />
-      <View style={styles.content}>
-        <View style={styles.aligner}>
-          <View>
-            <Text style={styles.title}>
-              {car.model.make.name} {car.model.name}, {car.releaseYear}
-            </Text>
-            <Text style={styles.price}>${car.price.toLocaleString()}</Text>
+    <>
+      <ScrollView style={styles.container}>
+        <Image source={{ uri: car.image }} style={styles.image} />
+        <View style={styles.content}>
+          <View style={styles.aligner}>
+            <View>
+              <Text style={styles.title}>
+                {car.model.make.name} {car.model.name}, {car.releaseYear}
+              </Text>
+              <Text style={styles.price}>${car.price.toLocaleString()}</Text>
+            </View>
+            <View>
+              <RoundedButton onPress={onPressHandler}>
+                {favorite ? (
+                  <MaterialIcons name="favorite" size={32} color="white" />
+                ) : (
+                  <MaterialIcons
+                    name="favorite-border"
+                    size={32}
+                    color="white"
+                  />
+                )}
+              </RoundedButton>
+            </View>
           </View>
-          <View>
-            <RoundedButton onPress={onPressHandler}>
-              {favorite ? (
-                <MaterialIcons name="favorite" size={32} color="white" />
-              ) : (
-                <MaterialIcons name="favorite-border" size={32} color="white" />
-              )}
-            </RoundedButton>
+          <Line />
+          <View style={styles.tiles}>
+            <Tile label="Mileage" value={`${car.mileage}km`}>
+              <MaterialIcons name="add-road" size={24} color="#1c1b1f" />
+            </Tile>
+            <Tile label="Gear" value={normalize(car.gearbox)}>
+              <MaterialCommunityIcons
+                name="car-shift-pattern"
+                size={24}
+                color="#1c1b1f"
+              />
+            </Tile>
+            <Tile label="Power" value={`${car.power}hp`}>
+              <MaterialIcons name="speed" size={24} color="#1c1b1f" />
+              {/* <MaterialCommunityIcons name="speedometer" size={24} color="#1c1b1f" /> */}
+            </Tile>
+            <Tile label="Fuel" value={normalize(car.fuel)}>
+              <MaterialIcons
+                name="local-gas-station"
+                size={24}
+                color="#1c1b1f"
+              />
+            </Tile>
           </View>
-        </View>
-        <Line />
-        <View style={styles.tiles}>
-          <Tile label="Mileage" value={`${car.mileage}km`}>
-            <MaterialIcons name="add-road" size={24} color="#1c1b1f" />
-          </Tile>
-          <Tile label="Gear" value={normalize(car.gearbox)}>
-            <MaterialCommunityIcons
-              name="car-shift-pattern"
-              size={24}
-              color="#1c1b1f"
+          <Line />
+          <View style={styles.statsList}>
+            <View style={styles.statsRow}>
+              <Text style={styles.statsLabel}>City</Text>
+              <Text style={styles.statsValue}>{car.city}</Text>
+            </View>
+            <View style={styles.statsRow}>
+              <Text style={styles.statsLabel}>Release Year</Text>
+              <Text style={styles.statsValue}>{car.releaseYear}</Text>
+            </View>
+            <View style={styles.statsRow}>
+              <Text style={styles.statsLabel}>Body Type</Text>
+              <Text style={styles.statsValue}>{normalize(car.bodyType)}</Text>
+            </View>
+            <View style={styles.statsRow}>
+              <Text style={styles.statsLabel}>Color</Text>
+              <Text style={styles.statsValue}>{normalize(car.color)}</Text>
+            </View>
+            <View style={styles.statsRow}>
+              <Text style={styles.statsLabel}>Condition</Text>
+              <Text style={styles.statsValue}>{normalize(car.condition)}</Text>
+            </View>
+            <View style={styles.statsRow}>
+              <Text style={styles.statsLabel}>New</Text>
+              <Text style={styles.statsValue}>{car.isNew ? 'Yes' : 'No'}</Text>
+            </View>
+            <View style={styles.statsRow}>
+              <Text style={styles.statsLabel}>Engine Size (L)</Text>
+              <Text style={styles.statsValue}>
+                {car.engineSize === 0 ? '-' : `${car.engineSize}L`}
+              </Text>
+            </View>
+          </View>
+          <Line />
+          {/* NOTE: JSON/TypeScript integration will be implemented later */}
+          <Text style={styles.addFeatTitle}>Additional Features</Text>
+          <ScrollView style={styles.addFeatures} horizontal={true}>
+            {car.addFeats.map((feat) => (
+              <Text key={feat} style={styles.feature}>
+                {normalize(feat)}
+              </Text>
+            ))}
+          </ScrollView>
+          <Line />
+          <View style={styles.aligner}>
+            <View>
+              <Text style={styles.username}>{car.seller.fullName}</Text>
+              <Text style={styles.phoneNumber}>{car.seller.phone}</Text>
+            </View>
+            <View>
+              <MaterialIcons name="person-outline" size={40} color="black" />
+            </View>
+          </View>
+          <Line />
+          <Pressable
+            style={({ pressed }) => [
+              styles.reportButton,
+              pressed && styles.pressed,
+            ]}
+          >
+            <MaterialIcons
+              name="warning-amber"
+              size={40}
+              color={Colors.primary}
             />
-          </Tile>
-          <Tile label="Power" value={`${car.power}hp`}>
-            <MaterialIcons name="speed" size={24} color="#1c1b1f" />
-            {/* <MaterialCommunityIcons name="speedometer" size={24} color="#1c1b1f" /> */}
-          </Tile>
-          <Tile label="Fuel" value={normalize(car.fuel)}>
-            <MaterialIcons name="local-gas-station" size={24} color="#1c1b1f" />
-          </Tile>
+            <Text style={styles.reportButtonText}>Report List</Text>
+          </Pressable>
         </View>
-        <Line />
-        <View style={styles.statsList}>
-          <View style={styles.statsRow}>
-            <Text style={styles.statsLabel}>City</Text>
-            <Text style={styles.statsValue}>{car.city}</Text>
-          </View>
-          <View style={styles.statsRow}>
-            <Text style={styles.statsLabel}>Release Year</Text>
-            <Text style={styles.statsValue}>{car.releaseYear}</Text>
-          </View>
-          <View style={styles.statsRow}>
-            <Text style={styles.statsLabel}>Body Type</Text>
-            <Text style={styles.statsValue}>{normalize(car.bodyType)}</Text>
-          </View>
-          <View style={styles.statsRow}>
-            <Text style={styles.statsLabel}>Color</Text>
-            <Text style={styles.statsValue}>{normalize(car.color)}</Text>
-          </View>
-          <View style={styles.statsRow}>
-            <Text style={styles.statsLabel}>Condition</Text>
-            <Text style={styles.statsValue}>{normalize(car.condition)}</Text>
-          </View>
-          <View style={styles.statsRow}>
-            <Text style={styles.statsLabel}>New</Text>
-            <Text style={styles.statsValue}>{car.isNew ? 'Yes' : 'No'}</Text>
-          </View>
-        </View>
-        <Line />
-        {/* NOTE: JSON/TypeScript integration will be implemented later */}
-        <Text style={styles.addFeatTitle}>Additional Features</Text>
-        <ScrollView style={styles.addFeatures} horizontal={true}>
-          {car.addFeats.map((feat) => (
-            <Text key={feat} style={styles.feature}>
-              {normalize(feat)}
-            </Text>
-          ))}
-        </ScrollView>
-        <Line />
-        <View style={styles.aligner}>
-          <View>
-            <Text style={styles.username}>{car.seller.fullName}</Text>
-            <Text style={styles.phoneNumber}>{car.seller.phone}</Text>
-          </View>
-          <View>
-            <MaterialIcons name="person-outline" size={40} color="black" />
-          </View>
-        </View>
-        <Line />
-        <Pressable
-          style={({ pressed }) => [
-            styles.reportButton,
-            pressed && styles.pressed,
-          ]}
-        >
-          <MaterialIcons
-            name="warning-amber"
-            size={40}
-            color={Colors.primary}
-          />
-          <Text style={styles.reportButtonText}>Report List</Text>
-        </Pressable>
-      </View>
-    </ScrollView>
+      </ScrollView>
+      <Pressable
+        style={({ pressed }) => [
+          {
+            /* NOTE: WORK IN PROGRESS */
+            /*           position: 'absolute',
+          bottom: 8, */
+            /*             marginBottom: 8,
+            marginHorizontal: 8,
+            borderRadius: 8, */
+            padding: 12,
+            width: 'auto',
+            backgroundColor: Colors.accent,
+            alignItems: 'center',
+          },
+          pressed && { opacity: 0.7 },
+        ]}
+      >
+        <Text style={{ fontFamily: Fonts.Satoshi.Black, color: Colors.white }}>
+          CALL THE SELLER
+        </Text>
+      </Pressable>
+    </>
   );
 }
 
